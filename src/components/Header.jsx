544bc2bc4714a7995fda7015d2e49abc9923664a
LaxmanZ <bhajantrilaxman2001@gmail.com>
2023-12-11 18:28:11 +0530

    designed the basic styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,38 @@
+import { Link } from 'react-router-dom';
+import Logo from '../assets/logo.png';
+import { FaBars } from 'react-icons/fa';
+import { AiOutlineClose } from 'react-icons/ai';
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <nav>
+      <div className="container nav-container">
+        <Link className="nav-logo" to={'/'}>
+          <img src={Logo} alt="logo" />
+        </Link>
+
+        <ul className="nav-menu">
+          <li>
+            <Link to={'/profile'}>Beg Joker</Link>
+          </li>
+          <li>
+            <Link to={'/create'}>Create Post</Link>
+          </li>
+          <li>
+            <Link to={'/author'}>Authors</Link>
+          </li>
+          <li>
+            <Link to={'/logout'}>Logout</Link>
+          </li>
+        </ul>
+
+        <button className="nav-toggle-button btn btn-primary">
+          <FaBars />
+          <AiOutlineClose />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
